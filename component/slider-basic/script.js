9b53cd955819b303656524ce51292bb2d8f4593e
thuantruong00 <46294831+thuantruong00@users.noreply.github.com>
2020-08-29 21:02:14 +0700

    js

---
+++ component/slider-basic/script.js
@@ -0,0 +1,130 @@
+$(document).ready(function(){
+    $('#num0').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navText:['<span>&#x3008;</span>','<span>&#x3009;</span>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+    })
+    $('#num1').owlCarousel({
+        loop:true,
+        margin:30,
+        nav:true,
+        dots:false,
+        center:true,
+        navText:['<span>&#x3008;</span>','<span>&#x3009;</span>'],
+        responsive:{
+            0:{
+                items:3
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+    $('#num2').owlCarousel({
+        loop:true,
+        margin:10,
+        dots:false,
+        center:true,
+        navText:['<span>&#x3008;</span>','<span>&#x3009;</span>'],
+        responsive:{
+            0:{
+                items:3
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+    $('#num3').owlCarousel({
+        loop:true,
+        margin:10,
+        dots:false,
+        center:true,
+        navText:['<span>&#x3008;</span>','<span>&#x3009;</span>'],
+        responsive:{
+            0:{
+                items:3
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+
+    windowSize = $(window).width();
+    setHeight();
+    $(window).resize(function() {
+        windowSize = $(window).width();
+        setHeight();
+    });
+    
+    function setHeight(){
+        if(windowSize<=991){
+            temp = $('#num1 .center').height();
+            temp*=2;
+            temp+='px';
+            $('#num1').css('height',temp);
+            $('#changeContainer').removeClass('container');
+            $('#changeContainer').addClass('container-fluid');
+        }else{
+            temp = $('#num1 .center').height();
+            temp+='px';
+            $('#num1').css('height',temp);
+            $('#changeContainer').removeClass('container-fluid');
+            $('#changeContainer').addClass('container');
+        }
+    }
+
+    temp2 = $('#num2 .center');
+    temp3=temp2.height();
+    temp3*=2;
+    temp3+='px';
+    $('#num2').css('height',temp3);
+    
+
+
+a = $('.center').prev();
+b = $(a).find('img');
+$('#num2 img').removeClass('c-scale');
+$(b[1]).addClass('c-scale'); 
+
+
+
+    // flag  = 0 ;
+    // $('#num2').mousedown((event)=>{
+    //     flag=1;
+    // });
+    // $(window).mouseup((event)=>{
+    //     if(flag==1){
+    //         //$('.center').prev().find('img').addClass('c-scale');
+    //         a = $('.center').prev();
+    //         b = $(a).find('img');
+    //         $('#num2 img').removeClass('c-scale');
+    //         $(b[1]).addClass('c-scale');        
+    //     }
+    //     flag=0;
+        
+    // });
+
+});
